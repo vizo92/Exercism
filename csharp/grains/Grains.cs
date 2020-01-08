819810f062fc8d2a575d80f3f28b40fd27d6602a
Sylwester Kwiatkowski <s.kwiatkowski1992@gmail.com>
2020-01-08 20:09:07 +0100

    first exercises

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if(n<=0 || n > 64)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        return Convert.ToUInt64(Math.Pow(2, n-1));
+    }
+
+    public static ulong Total()
+    {
+        var list = Enumerable.Range(1,64).Select(x=> Square(x)).ToList();
+        return list.Aggregate((a,c) => a+c);
+
+    }
+}
