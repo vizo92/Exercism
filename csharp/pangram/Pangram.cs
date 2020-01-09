8ae1596f21c7b032aba89f3250eae801c311a6b1
Sylwester Kwiatkowski <s.kwiatkowski1992@gmail.com>
2020-01-09 21:25:25 +0100

    Pangram solution

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g' ,'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        char[] charList = input.ToLower().ToCharArray();
+
+        foreach (char letter in alphabet)
+        {
+            if(!charList.Contains(letter)) return false;
+        }
+        return true;
     }
 }
