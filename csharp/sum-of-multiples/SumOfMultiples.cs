34e312662d259b1ad5f4aa3d5caf943142488ea4
Sylwester Kwiatkowski <s.kwiatkowski1992@gmail.com>
2020-01-10 15:58:29 +0100

    SumOfMultiples solution

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        List<int> toSumUp = new List<int>();
+
+        foreach (int num in multiples)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                if (num * i >= max)
+                {
+                    break;
+                }
+                else
+                {
+                    toSumUp.Add(num * i);
+                }
+            }
+        }
+
+        return toSumUp.Distinct().Sum();
+    }
+}
